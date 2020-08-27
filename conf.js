7fb85d88cd6304e3d95afc50561c0c8422a98ac6
fcki1984 <39592566+fcki1984@users.noreply.github.com>
2020-08-27 16:09:47 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -28,7 +28,7 @@ jsproxy_config({
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        //'b.007.workers.dev': 1,
+        'blue-moon-8c65.kclds4.workers.dev': 1,
         //'b.hehe.workers.dev': 1,
         //'b.lulu.workers.dev': 1,
         //'b.jsproxy.workers.dev': 1,
